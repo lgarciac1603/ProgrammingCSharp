99e69753dc9421adaf8b581e456337bddcdf4d55
lgarciac1603 <lfgc851@gmail.com>
2025-03-31 14:23:44 -0600

    assignment finished

---
+++ ProgrammingCSharp/Classes&Objects/Practice_2_M3.cs
@@ -0,0 +1,30 @@
+﻿namespace ProgrammingCSharp.Classes_Objects
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Greet()
+        {
+            Console.WriteLine($"Hello, my name is {Name}");
+        }
+    }
+
+    internal class Practice_2_M3
+    {
+        public static void Main(string[] args)
+        {
+            Person friend = new Person();
+            friend.Name = "John";
+            friend.Age = 25;
+
+            Person collage = new Person();
+            collage.Name = "Jane";
+            collage.Age = 22;
+
+            friend.Greet();
+            collage.Greet();
+        }
+    }
+}
