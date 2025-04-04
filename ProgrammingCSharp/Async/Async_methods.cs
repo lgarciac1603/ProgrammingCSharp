08514e52706af82e26acea5ae3f22c81f15eb84d
lgarciac1603 <lfgc851@gmail.com>
2025-04-04 08:54:35 -0600

    Async methods

---
+++ ProgrammingCSharp/Async/Async_methods.cs
@@ -0,0 +1,36 @@
+﻿namespace ProgrammingCSharp.Async
+{
+    public class Async_methods
+    {
+        public async Task DownloadDataAsync()
+        {
+            try
+            {
+                Console.WriteLine("Download started...");
+                throw new InvalidOperationException("Simulated download error.");
+                await Task.Delay(3000);
+                Console.WriteLine("Download completed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+        public async Task DownloadDataAsync2()
+        {
+            Console.WriteLine("Download 2 started...");
+            await Task.Delay(2000);
+            Console.WriteLine("Download 2 completed.");
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Async_methods program = new Async_methods();
+            Task task1 = program.DownloadDataAsync();
+            Task task2 = program.DownloadDataAsync2();
+            await Task.WhenAll(task1, task2);
+            Console.WriteLine("All downloads completed.");
+        }
+    }
+}
